95e20827031dfbed35fa56df08c80ab40d46bc09
eminecetin <eminecetinn970@gmail.com>
2024-04-15 14:56:00 +0300

    nlp-service installed

---
+++ nlp-service/src/main/java/com/booking/nlpservice/NlpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.booking.nlpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlpServiceApplication.class, args);
+	}
+
+}
---
+++ nlp-service/src/test/java/com/booking/nlpservice/NlpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.nlpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
